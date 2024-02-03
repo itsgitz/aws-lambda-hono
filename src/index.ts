cb024a0f23f491f13335e3bdc11799dbdc18917f
itsgitz <anggit.ginanjar.dev@gmail.com>
2024-02-03 13:40:06 +0700

    feat: added another route

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,20 @@ import { handle } from 'hono/aws-lambda'
 
 const app = new Hono()
 
-app.get('/', (c) => {
+app.get('/', async (c) => {
   console.log('context', c);
-  return c.text('Hello from hono here!')
+  return c.json({
+    statusCode: 200,
+    message: 'Hono application in AWS Lambda (no CDK)'
+  });
+})
+
+app.get('/hello', async (c) => {
+  console.log('context', c);
+  return c.json({
+    statusCode: 200,
+    message: 'You mean hello world?'
+  });
 })
 
 export const handler = handle(app)
